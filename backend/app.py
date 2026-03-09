25c1a2ff75b82b29dc746a5b206e9b269e6f8911
manojym73 <manojym73@gmail.com>
2026-03-09 23:36:06 +0530

    Connected Flask backend with MySQL database

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,26 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Crop Trading API Running"
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 from flask import Flask
+from database import get_connection
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Crop Trading API Running"
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+
+    return f"Database connected! Tables: {tables}"
 
 if __name__ == "__main__":
     app.run(debug=True)
