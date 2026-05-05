18b912184a552091bc1708da1631c136f68a343d
manojym73 <manojym73@gmail.com>
2026-05-05 16:12:05 +0530

    fix: resolve major project errors

--- backend/app.py
+++ backend/app.py
@@ -729,5 +729,8 @@ def delete_salesman_order():
     
     
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,1 +1,6 @@
-const API = "http://127.0.0.1:5000";
+// const API = "http://127.0.0.1:5000";
+const API = "http://127.0.0.1:5001";
+
+window.fetchAPI = (endpoint, options = {}) => {
+  return fetch(`${API}/${endpoint}`, options);
+};
